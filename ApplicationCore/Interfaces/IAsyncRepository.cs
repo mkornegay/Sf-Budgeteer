e924c382183d121bcef2a5ff69c629391de9c851
Mike Kornegay <mjkorneg@gmail.com>
2018-11-04 17:05:10 -0500

    Added interfaces for Async Repository and Specification

---
+++ ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+﻿using Sf_Budgeteer_ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sf_Budgeteer_ApplicationCore.Interfaces
+{
+    public interface IAsyncRepository<T> where T: BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> ListAllAsync();
+        Task<List<T>> ListAsync(ISpecification specification);
+    }
+}
