19073265222b9a0256e5d23e9bead8203709ed8e
Mike Kornegay <mjkorneg@gmail.com>
2018-11-09 11:18:24 -0500

    Add synchronous repository

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sf_Budgeteer_ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sf_Budgeteer_ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+
+        T GetById(int id);
+        T GetSingleBySpec(ISpecification<T> spec);
+        IEnumerable<T> ListAll();
+        IEnumerable<T> List(ISpecification<T> spec);
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
